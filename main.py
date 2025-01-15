2cafb7b0c885761e0af90633275b3a2b05145c29
Abdul Wasae <wasaeinfinity@gmail.com>
2025-01-15 19:13:38 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,323 @@
+import random
+stages = [r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+logo = r''' 
+ _                                             
+| |                                            
+| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                    __/ |                      
+                   |___/    '''
+word_list = [
+    'abruptly',
+    'absurd',
+    'abyss',
+    'affix',
+    'askew',
+    'avenue',
+    'awkward',
+    'axiom',
+    'azure',
+    'bagpipes',
+    'bandwagon',
+    'banjo',
+    'bayou',
+    'beekeeper',
+    'bikini',
+    'blitz',
+    'blizzard',
+    'boggle',
+    'bookworm',
+    'boxcar',
+    'boxful',
+    'buckaroo',
+    'buffalo',
+    'buffoon',
+    'buxom',
+    'buzzard',
+    'buzzing',
+    'buzzwords',
+    'caliph',
+    'cobweb',
+    'cockiness',
+    'croquet',
+    'crypt',
+    'curacao',
+    'cycle',
+    'daiquiri',
+    'dirndl',
+    'disavow',
+    'dizzying',
+    'duplex',
+    'dwarves',
+    'embezzle',
+    'equip',
+    'espionage',
+    'euouae',
+    'exodus',
+    'faking',
+    'fishhook',
+    'fixable',
+    'fjord',
+    'flapjack',
+    'flopping',
+    'fluffiness',
+    'flyby',
+    'foxglove',
+    'frazzled',
+    'frizzled',
+    'fuchsia',
+    'funny',
+    'gabby',
+    'galaxy',
+    'galvanize',
+    'gazebo',
+    'giaour',
+    'gizmo',
+    'glowworm',
+    'glyph',
+    'gnarly',
+    'gnostic',
+    'gossip',
+    'grogginess',
+    'haiku',
+    'haphazard',
+    'hyphen',
+    'iatrogenic',
+    'icebox',
+    'injury',
+    'ivory',
+    'ivy',
+    'jackpot',
+    'jaundice',
+    'jawbreaker',
+    'jaywalk',
+    'jazziest',
+    'jazzy',
+    'jelly',
+    'jigsaw',
+    'jinx',
+    'jiujitsu',
+    'jockey',
+    'jogging',
+    'joking',
+    'jovial',
+    'joyful',
+    'juicy',
+    'jukebox',
+    'jumbo',
+    'kayak',
+    'kazoo',
+    'keyhole',
+    'khaki',
+    'kilobyte',
+    'kiosk',
+    'kitsch',
+    'kiwifruit',
+    'klutz',
+    'knapsack',
+    'larynx',
+    'lengths',
+    'lucky',
+    'luxury',
+    'lymph',
+    'marquis',
+    'matrix',
+    'megahertz',
+    'microwave',
+    'mnemonic',
+    'mystify',
+    'naphtha',
+    'nightclub',
+    'nowadays',
+    'numbskull',
+    'nymph',
+    'onyx',
+    'ovary',
+    'oxidize',
+    'oxygen',
+    'pajama',
+    'peekaboo',
+    'phlegm',
+    'pixel',
+    'pizazz',
+    'pneumonia',
+    'polka',
+    'pshaw',
+    'psyche',
+    'puppy',
+    'puzzling',
+    'quartz',
+    'queue',
+    'quips',
+    'quixotic',
+    'quiz',
+    'quizzes',
+    'quorum',
+    'razzmatazz',
+    'rhubarb',
+    'rhythm',
+    'rickshaw',
+    'schnapps',
+    'scratch',
+    'shiv',
+    'snazzy',
+    'sphinx',
+    'spritz',
+    'squawk',
+    'staff',
+    'strength',
+    'strengths',
+    'stretch',
+    'stronghold',
+    'stymied',
+    'subway',
+    'swivel',
+    'syndrome',
+    'thriftless',
+    'thumbscrew',
+    'topaz',
+    'transcript',
+    'transgress',
+    'transplant',
+    'triphthong',
+    'twelfth',
+    'twelfths',
+    'unknown',
+    'unworthy',
+    'unzip',
+    'uptown',
+    'vaporize',
+    'vixen',
+    'vodka',
+    'voodoo',
+    'vortex',
+    'voyeurism',
+    'walkway',
+    'waltz',
+    'wave',
+    'wavy',
+    'waxy',
+    'wellspring',
+    'wheezy',
+    'whiskey',
+    'whizzing',
+    'whomever',
+    'wimpy',
+    'witchcraft',
+    'wizard',
+    'woozy',
+    'wristwatch',
+    'wyvern',
+    'xylophone',
+    'yachtsman',
+    'yippee',
+    'yoked',
+    'youthful',
+    'yummy',
+    'zephyr',
+    'zigzag',
+    'zigzagging',
+    'zilch',
+    'zipper',
+    'zodiac',
+    'zombie',
+]
+                   
+                   
+print(logo)
+
+word=random.choice(word_list)
+print(word)
+
+lives=6
+game=True
+correct_letters=[]
+
+while game:
+    display=""
+    guess=input("enter your guess: ")
+    for i in word:
+        if i==guess:
+            display+=guess
+            correct_letters.append(guess)
+        elif i in correct_letters:
+            display+=i
+        else:
+         display+="_"
+    print(display)
+    
+    if guess in correct_letters:
+        print("you already entered this letter")
+    
+    if guess not in word:
+        lives-=1
+        print("wrong guess...you lost a life")
+        if lives==0:
+            print("you lose.")
+            game=False
+    
+    if display==word:
+        print("you won.")
+        game=False
+    
+    print(stages[lives])    
+        
+            
